e9c987eac79f8d99ed33385b6f87d6623512ce10
uns111code <uns.code111@gmail.com>
2025-03-17 17:09:46 +0100

    first commit

---
+++ vue-compteur/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <span class="counter-title">Compteur Title</span>
+    <div class='container'>
+        
+      <div class='counter-container' >
+        <span class="counter-display">0</span>
+      </div>
+
+      <div class='buttons-container'>
+          <button id='moins'>-</button>
+          <button id='plus'>+</button>
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped>
+</style>
