0693e487fe536c3592f2f7bd9c51e87a98ea95cb
uns111code <uns.code111@gmail.com>
2025-03-21 17:00:15 +0100

    vendredi

---
+++ watchlist/src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import SeriesCard from "./components/SeriesCard.vue";
+
+import { userWatchList } from "./assets/js/watchList.js";
+
+import { ref } from "vue";
+
+const watchList = ref(userWatchList);
+
+console.log(watchList)
+</script>
+
+<template>
+  <div class="container">
+      <section class="sidebar">
+          <ul>
+              <li>Ici c'est fixe</li>
+              <li>Un lien de nav 1</li>
+              <li>Un lien de nav 2</li>
+          </ul>
+          <span>▶ Lecture en cours: (ep title) </span>
+      </section>
+      <main class="courses-container">
+        <SeriesCard :userWatchList="userWatchList" />
+      </main>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ watchlist/src/assets/js/watchList.js
@@ -0,0 +1,26 @@
+export const userWatchList = [
+    {
+        "series":"Better Call Saul",
+        "season":4,
+        "episode":8,
+        "timeCursor":"00:42:15",
+        "title":"So long Mike"
+
+    },
+    {
+        "series":"The Leftovers",
+        "season":2,
+        "episode":2,
+        "timeCursor":"00:12:15",
+        "title":"Jerusalem"
+
+    },
+    {
+        "series":"The OA",
+        "season":1,
+        "episode":3,
+        "timeCursor":"00:00:00",
+        "title":"Angel"
+
+    }
+]
